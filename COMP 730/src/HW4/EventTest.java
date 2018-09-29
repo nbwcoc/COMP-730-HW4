c0cd564466e4d25e8d0351cfa9dcfb92d7ba7dcb
John Smith <nbwcoc@openmailbox.org>
2018-09-29 16:19:42 -0400

    EventTest

---
+++ COMP 730/src/HW4/EventTest.java
@@ -0,0 +1,32 @@
+package HW4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+class EventTest {
+
+    Event testObject;
+    int testNumber;
+    int testData;
+    
+    EventTest() {
+        var rand = new Random();
+        testNumber = rand.nextInt();
+        testData = rand.nextInt();
+        testObject = new Event(testNumber, testData);
+    }
+
+    @Test
+    void testGetEventData() {
+        assertEquals(testObject.getEventData(), testData);
+    }
+
+    @Test
+    void testGetEventNumber() {
+        assertEquals(testObject.getEventNumber(), testNumber);
+    }
+
+}
