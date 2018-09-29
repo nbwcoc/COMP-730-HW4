6e14bf0d9adee9179db32669287c5305d1f4215c
John Smith <nbwcoc@openmailbox.org>
2018-09-29 15:35:51 -0400

    complete PublisherImplementation

--- COMP 730/src/HW4/PublisherImplementation.java
+++ COMP 730/src/HW4/PublisherImplementation.java
@@ -1,31 +1,37 @@
 package HW4;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Random;
 
 public class PublisherImplementation implements PublisherInterface {
-    private HashSet<Observer> Observers = new HashSet<Observer>();
-
+    private HashSet<Observer> observers = new HashSet<Observer>();
+    private HashMap<Observer, Integer> counters = new HashMap<Observer, Integer>();
+    int eventCounter = 0;
+    
     @Override
     public void registerObserver(Observer o) {
         // TODO Auto-generated method stub
-
+        observers.add(o);
     }
 
     @Override
     public void removeObserver(Observer o) {
         // TODO Auto-generated method stub
-
+        observers.remove(o);
+        counters.remove(o);
     }
 
     @Override
-    public void notifyObservers(Observer o) {
+    public void notifyObservers(Event e) {
         // TODO Auto-generated method stub
-
+        for (var observer : observers)
+            if (observer.notifyObserver(e))
+                counters.put(observer, counters.getOrDefault(observer, 0) + 1);
     }
     
     private Event generateEvent() {
-        return new Event(0, 0);
+        return new Event(eventCounter++, new Random().nextInt(5000) + 1);
     }
     
     public void runSimulation() {
@@ -39,8 +45,19 @@ public class PublisherImplementation implements PublisherInterface {
         var rand = new Random();
         
         for (int i = 0; i < 200; i++) {
-            var e = new Event(i, rand.nextInt(5000) + 1);
-            ;
+            notifyObservers(generateEvent());
+            
+            if (i % 40 == 0) {
+                if (!observers.contains(odds))
+                    registerObserver(odds);
+                if (!observers.contains(threes))
+                    registerObserver(threes);
+            }
+            
+            if (counters.getOrDefault(odds, 0) > 20)
+                removeObserver(odds);
+            if (counters.getOrDefault(threes, 0) > 6)
+                removeObserver(threes);
         }
     }
 }
--- COMP 730/src/HW4/PublisherInterface.java
+++ COMP 730/src/HW4/PublisherInterface.java
@@ -3,5 +3,5 @@ package HW4;
 public interface PublisherInterface {
     public void registerObserver(Observer o);
     public void removeObserver(Observer o);
-    public void notifyObservers(Observer o);
+    public void notifyObservers(Event e);
 }
